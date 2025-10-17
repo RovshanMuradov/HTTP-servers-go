b902b3404ad4bbb48a333b6609db6d4c798e989e
Rovshan Muradov <63051606+RovshanMuradov@users.noreply.github.com>
2025-10-17 10:07:42 +0400

    CH2: Routing, L1: Stateful Handlers

--- main.go
+++ main.go
@@ -1,17 +1,64 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"sync/atomic"
 )
 
-func main() {
-	mux := http.NewServeMux()
+type apiConfig struct {
+	fileserverHits atomic.Int32
+}
 
-	server := &http.Server{
-		Addr:    ":8080",
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits.Add(1)
+		next.ServeHTTP(w, r)
+	})
+}
+
+func (cfg *apiConfig) handlerMetrics(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plan; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprintf(w, "Hits: %d", cfg.fileserverHits.Load())
+}
+
+func (cfg *apiConfig) handlerReset(w http.ResponseWriter, r *http.Request) {
+	cfg.fileserverHits.Store(0)
+	w.WriteHeader(http.StatusOK)
+}
+
+func (cfg *apiConfig) handleHealth(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8") // normal header
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("OK"))
+}
+
+func main() {
+
+	const filepathRoot = "."
+	const port = "8080"
+
+	mux := http.NewServeMux()
+	apiCfg := &apiConfig{}
+
+	mux.Handle("/app/", apiCfg.middlewareMetricsInc(
+		http.StripPrefix("/app", http.FileServer(http.Dir(filepathRoot))),
+	))
+	mux.HandleFunc("/metrics", apiCfg.handlerMetrics)
+	mux.HandleFunc("/reset", apiCfg.handlerReset)
+	mux.HandleFunc("/healthz", apiCfg.handleHealth)
+
+	mux.Handle("/app/assets/", apiCfg.middlewareMetricsInc(
+		http.StripPrefix("/app/assets", http.FileServer(http.Dir("./assets"))),
+	))
+
+	srv := &http.Server{
+		Addr:    ":" + port,
 		Handler: mux,
 	}
 
-	log.Fatal(http.ListenAndServe(server.Addr, server.Handler))
+	log.Printf("Serving files from %s on port: %s\n", filepathRoot, port)
+	log.Fatal(srv.ListenAndServe())
 }
