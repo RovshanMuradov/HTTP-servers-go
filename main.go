a28e2d6ffaeb5a77008b02b8fbd1f593cd8c0a6b
Rovshan Muradov <63051606+RovshanMuradov@users.noreply.github.com>
2025-11-10 07:48:53 +0400

    CH5: Storage, L5: Create User

--- main.go
+++ main.go
@@ -1,16 +1,26 @@
 package main
 
 import (
+	"database/sql"
 	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 	"strings"
 	"sync/atomic"
+	"time"
+
+	"github.com/google/uuid"
+	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
+	"github.com/rovshanmuradov/HTTP-servers-go/internal/database"
 )
 
 type apiConfig struct {
 	fileserverHits atomic.Int32
+	DB             *database.Queries
+	Platform       string
 }
 
 // middlewareMetricsInc is a middleware that increments the file server hit counter.
@@ -37,6 +47,17 @@ func (cfg *apiConfig) handlerMetrics(w http.ResponseWriter, r *http.Request) {
 
 // handlerReset handles the /api/reset endpoint.
 func (cfg *apiConfig) handlerReset(w http.ResponseWriter, r *http.Request) {
+
+	if cfg.Platform != "dev" {
+		respondWithError(w, 403, "Not dev")
+		return
+	}
+	err := cfg.DB.DeleteAllUsers(r.Context())
+	if err != nil {
+		respondWithError(w, 400, "Failed to delete users")
+		return
+	}
+
 	cfg.fileserverHits.Store(0)
 	w.WriteHeader(http.StatusOK)
 }
@@ -97,13 +118,55 @@ func respondWithJSON(w http.ResponseWriter, code int, payload interface{}) {
 	w.Write(dat)
 }
 
+type User struct {
+	ID        uuid.UUID `json:"id"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+	Email     string    `json:"email"`
+}
+
+func (cfg *apiConfig) createUser(w http.ResponseWriter, r *http.Request) {
+	type parameters struct {
+		Email string `json:"email"`
+	}
+
+	decoder := json.NewDecoder(r.Body)
+	params := parameters{}
+	err := decoder.Decode(&params)
+	if err != nil {
+		respondWithError(w, 400, "Something went wrong")
+		return
+	}
+	user, err := cfg.DB.CreateUser(r.Context(), sql.NullString{String: params.Email, Valid: true})
+	if err != nil {
+		respondWithError(w, 400, "Failed to create user")
+		return
+	}
+	respondWithJSON(w, 201, User{ID: user.ID.UUID, CreatedAt: user.CreatedAt.Time, UpdatedAt: user.UpdatedAt.Time, Email: user.Email.String})
+
+}
+
 func main() {
+
+	if err := godotenv.Load(); err != nil {
+		log.Fatal(err)
+	}
+	platform := os.Getenv("PLATFORM")
+	dbURL := os.Getenv("DB_URL")
+	db, err := sql.Open("postgres", dbURL)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
 
 	const filepathRoot = "."
 	const port = "8080"
 
 	mux := http.NewServeMux()
-	apiCfg := &apiConfig{}
+	apiCfg := &apiConfig{
+		DB:       database.New(db),
+		Platform: platform,
+	}
 
 	mux.Handle("/app/", apiCfg.middlewareMetricsInc(
 		http.StripPrefix("/app/", http.FileServer(http.Dir(filepathRoot))),
@@ -112,6 +175,7 @@ func main() {
 	mux.HandleFunc("GET /admin/metrics", apiCfg.handlerMetrics)
 	mux.HandleFunc("POST /admin/reset", apiCfg.handlerReset)
 	mux.HandleFunc("POST /api/validate_chirp", apiCfg.handlerValidateChirp)
+	mux.HandleFunc("POST /api/users", apiCfg.createUser)
 
 	srv := &http.Server{
 		Addr:    ":" + port,
